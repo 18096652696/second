281da8b73a397af399fcf66462953b9eb3e5f707
jackson <18096652696@163.com>
2018-07-02 15:14:31 +0800

    更新单个模块

---
+++ src/com/shsxt/service/UserService.java
@@ -0,0 +1,12 @@
+package com.shsxt.service;
+
+/**
+ * Created by Administrator on 2018/7/2 0002.
+ */
+public class UserService {
+
+    public static void main(String[] args) {
+
+        System.out.println("接受并查询");
+    }
+}
