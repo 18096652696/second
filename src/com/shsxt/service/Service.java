0ad42eb78a7dc5feec3570ebac5986766e1ab521
jackson <18096652696@163.com>
2018-07-02 16:40:47 +0800

    分支更新提交

---
+++ src/com/shsxt/service/Service.java
@@ -0,0 +1,12 @@
+package com.shsxt.service;
+
+/**
+ * Created by Administrator on 2018/7/2 0002.
+ */
+public class Service {
+
+    public static void main(String[] args) {
+
+        System.out.println("分支的bean");
+    }
+}
