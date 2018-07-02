d7e6cdc06e7a5a2d294b61657716fb4e5490590e
jackson <18096652696@163.com>
2018-07-02 17:57:08 +0800

    更新提交

--- src/com/shsxt/service/Service.java
+++ src/com/shsxt/service/Service.java
@@ -8,5 +8,6 @@ public class Service {
     public static void main(String[] args) {
 
         System.out.println("分支的bean");
+        System.out.println("master修改文件");
     }
 }
--- src/com/shsxt/service/Service01.java
+++ src/com/shsxt/service/Service01.java
@@ -10,6 +10,7 @@ public class Service01 {
     public static void main(String[] args) {
 
         System.out.println("woshi");
-        System.out.println("用户05修改文件");
+        System.out.println("bracnch修改文件");
+
     }
 }
