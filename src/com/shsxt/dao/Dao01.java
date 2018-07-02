ddddb8809e0e81c294e0c5870e51dce0eb45ebac
jackson <18096652696@163.com>
2018-07-02 17:11:14 +0800

    branch01分支提交

---
+++ src/com/shsxt/dao/Dao01.java
@@ -0,0 +1,10 @@
+package com.shsxt.dao;
+
+/**
+ * Created by Administrator on 2018/7/2 0002.
+ */
+public class Dao01 {
+    public static void main(String[] args) {
+        System.out.println("nishi");
+    }
+}
---
+++ src/com/shsxt/service/Service01.java
@@ -0,0 +1,14 @@
+package com.shsxt.service;
+
+import java.net.SocketTimeoutException;
+
+/**
+ * Created by Administrator on 2018/7/2 0002.
+ */
+public class Service01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("woshi");
+    }
+}
